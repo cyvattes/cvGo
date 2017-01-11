71adb75ca7dcfa8ce7da439f67beb1da6e7eaa9e
Cyrus <cy.vattes@gmail.com>
2017-01-11 21:23:10 +0000

    Add basic listener and response handler

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import ()
+import (
+	"net/http"
+)
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
 
 func main() {
-
+	http.HandleFunc("/", pageHandler)
+	http.ListenAndServe(":8080", nil)
 }
